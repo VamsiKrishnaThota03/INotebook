9cce9e1acdb781756068caf5e52158e61c1b2138
vamsi <vamsikrishnathota483@gmail.com>
2024-07-09 23:27:12 +0530

    commit all

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/inotebookDb";
+
+const connectToMongo= async()=>{
+    await mongoose.connect(mongoURI)
+    console.log("Mongoose is connected");
+}
+
+module.exports = connectToMongo
--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
